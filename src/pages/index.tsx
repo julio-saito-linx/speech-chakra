dd21df091bfc76d48e2b94aa1e7c5dc4b9809e08
Julio Makdisse Saito <julio@latife.com.br>
2021-05-01 17:38:39 -0300

    change speech rate

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,38 +1,103 @@
-import { Textarea } from "@chakra-ui/react";
+import {
+  FormControl,
+  FormLabel,
+  Slider,
+  SliderFilledTrack,
+  SliderThumb,
+  SliderTrack,
+  Textarea,
+} from "@chakra-ui/react";
 import { Button } from "@chakra-ui/button";
 import { Select } from "@chakra-ui/react";
 import { Container } from "../components/Container";
 import { Main } from "../components/Main";
 import { DarkModeSwitch } from "../components/DarkModeSwitch";
-import { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useSpeechSynthesis } from "react-speech-kit";
 
 const Index = () => {
   const [selectedVoice, selectedVoiceSet] = useState<SpeechSynthesisVoice>();
-  const [texto, textoSet] = useState("");
   const { speak, voices, cancel, speaking } = useSpeechSynthesis();
 
-  return (
-    <Container height="50vh">
-      <Main>
-        <Select
-          placeholder="Select option"
-          onChange={(ev) => {
-            selectedVoiceSet(
-              (voices as SpeechSynthesisVoice[]).find(
-                (v) => v.name === ev.target.value
-              )
-            );
-          }}
-        >
-          {voices?.map((v: SpeechSynthesisVoice) => (
-            <option key={v.name} value={v.name}>
-              {v.name}
-            </option>
-          ))}
-        </Select>
+  const [texto, textoSet] = useState(
+    "Um canalha ataca pelas costas, e Bolsonaro atacou o presidente da Petrobras, como sempre, da segurança do seu chiqueirinho à porta do Alvorada (ou pelas redes sociais). Disse que o brilhante executivo não trabalhava havia 11 meses porque, idoso, está em “home office”."
+  );
+  const [rate, rateSet] = useState(1);
+  const [minRate, minRateSet] = useState(0);
+  const [maxRate, maxRateSet] = useState(2);
 
-        <Textarea onChange={(ev) => textoSet(ev.target.value)} value={texto} />
+  useEffect(() => {
+    if (selectedVoice?.name?.match(/Google/g)) {
+      const MAX_RATE = 2;
+      minRateSet(0);
+      const percentRate = rate / maxRate;
+      rateSet(percentRate * MAX_RATE);
+      maxRateSet(MAX_RATE);
+    } else if (selectedVoice?.name?.match(/Microsoft/g)) {
+      const MAX_RATE = 10;
+      minRateSet(0);
+      const percentRate = rate / maxRate;
+      rateSet(percentRate * MAX_RATE);
+      maxRateSet(MAX_RATE);
+    }
+  }, [selectedVoice]);
+
+  return (
+    <Container height="10vh">
+      <Main>
+        <FormControl id="texto">
+          <FormLabel>Texto</FormLabel>
+          <Textarea
+            onChange={(ev) => textoSet(ev.target.value)}
+            value={texto}
+          />
+        </FormControl>
+
+        <FormControl id="voz">
+          <FormLabel>Voz</FormLabel>
+          <Select
+            placeholder="Select option"
+            onChange={(ev) => {
+              cancel();
+              selectedVoiceSet(
+                (voices as SpeechSynthesisVoice[]).find(
+                  (v) => v.name === ev.target.value
+                )
+              );
+            }}
+          >
+            {voices
+              ?.filter((v) => v.lang.indexOf("pt-BR") >= 0)
+              ?.map((v: SpeechSynthesisVoice) => (
+                <option key={v.name} value={v.name}>
+                  {v.name}
+                </option>
+              ))}
+          </Select>
+        </FormControl>
+
+        <FormControl id="velocidade">
+          <FormLabel>Velocidade</FormLabel>
+          <Slider
+            aria-label="slider-ex-1"
+            value={rate}
+            min={minRate}
+            max={maxRate}
+            step={0.01}
+            onChange={(value) => {
+              rateSet(value);
+            }}
+          >
+            <SliderTrack>
+              <SliderFilledTrack />
+            </SliderTrack>
+            <SliderThumb />
+          </Slider>
+          {new Intl.NumberFormat("pt-BR", {
+            maximumSignificantDigits: 3,
+          }).format(rate)}{" "}
+          ({minRate}/{maxRate})
+        </FormControl>
 
         <Button
           onClick={() => {
@@ -42,9 +107,9 @@ const Index = () => {
               speak({
                 text: texto,
                 voice: selectedVoice,
-                rate: 5,
-                pitch: 0.8,
-                volume: 0.4,
+                rate,
+                pitch: 1,
+                volume: 1,
               });
             }
           }}
